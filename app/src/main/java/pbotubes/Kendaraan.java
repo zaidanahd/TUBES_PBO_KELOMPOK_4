8c3b9096b8ee8395a09bc2924c87dea976f11382
Intanjeno02 <jenjenleeo23@gmail.com>
2026-05-24 15:17:04 +0700

    Intan-epic2

---
+++ app/src/main/java/pbotubes/Kendaraan.java
@@ -0,0 +1,19 @@
+package pbotubes.app;
+
+public class Kendaraan {
+    String platNomor;
+    String jenis;       
+    double hargaSewa;    
+    String merk;
+    String infoTambahan; 
+    String status;       
+ 
+    Kendaraan(String platNomor, String jenis, double hargaSewa, String merk, String infoTambahan) {
+    this.platNomor    = platNomor;
+    this.jenis        = jenis;
+    this.hargaSewa    = hargaSewa;
+    this.merk         = merk;
+    this.infoTambahan = infoTambahan;
+    this.status       = "Tersedia"; 
+    }
+}
